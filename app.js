d8f6e45be927be688f74a0eb39048a30ee4f9b41
Jack Gao <jack@v1021-wn-226-51.campus-dynamic.uwaterloo.ca>
2014-09-20 19:32:42 -0400

    s

--- app.js
+++ app.js
@@ -158,6 +158,7 @@ io.on('connection', function (socket) {
   });
 
   socket.on('getEnemyRoster', function (data) {
+    console.log("sabrina is a poopoo head");
     User.getEnemyRoster(data.enemyEmail, socket);
   });
 
