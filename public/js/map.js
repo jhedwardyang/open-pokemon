875cb9c5c5a8b5e546d638708c0d0f30bb99acfc
Edward Yang <jh.edwardyang@gmail.com>
2014-09-20 04:08:37 -0400

    audio library

--- public/js/map.js
+++ public/js/map.js
@@ -121,3 +121,13 @@ $(function(){
 
 
 
+
+ createjs.Sound.alternateExtensions = ["mp3"];
+ createjs.Sound.addEventListener("fileload", createjs.proxy(this.loadHandler, this));
+ createjs.Sound.registerSound("/audio/battle.ogg", "sound");
+ function loadHandler(event) {
+     // This is fired for each sound that is registered.
+     var instance = createjs.Sound.play("sound");  // play using id.  Could also use full sourcepath or event.src.
+     instance.addEventListener("complete", createjs.proxy(this.handleComplete, this));
+     instance.volume = 0.5;
+ }
